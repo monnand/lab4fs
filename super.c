555da6e067831e88808bc172f85215bb595169a1
Monnand <monnand@gmail.com>
2011-11-27 04:18:07 -0500

    fixing syntax errors

--- super.c
+++ super.c
@@ -86,94 +86,92 @@ struct inode *lab4fs_get_inode(struct super_block *sb, int mode, dev_t dev)
 
 static int lab4fs_fill_super(struct super_block * sb, void * data, int silent)
 {
-	struct buffer_head * bh;
-	int blocksize = BLOCK_SIZE;
-	unsigned long logic_sb_block;
-	unsigned offset = 0;
-	unsigned long sb_block = 1;
-	struct lab4fs_super_block *es;
-	struct lab4fs_sb_info *sbi;
-	struct inode *root;
-	int hblock;
+    struct buffer_head * bh;
+    int blocksize = BLOCK_SIZE;
+    unsigned long logic_sb_block;
+    unsigned offset = 0;
+    unsigned long sb_block = 1;
+    struct lab4fs_super_block *es;
+    struct lab4fs_sb_info *sbi;
+    struct inode *root;
+    int hblock;
 
     LAB4DEBUG("OK starting fill super...\n");
 
-	sbi = kmalloc(sizeof(*sbi), GFP_KERNEL);
-	if (!sbi)
-		return -ENOMEM;
+    sbi = kmalloc(sizeof(*sbi), GFP_KERNEL);
+    if (!sbi)
+        return -ENOMEM;
 
-	sb->s_fs_info = sbi;
-	memset(sbi, 0, sizeof(*sbi));
-	blocksize = sb_min_blocksize(sb, BLOCK_SIZE);
-	if (!blocksize) {
-		LAB4ERROR("unable to set blocksize\n");
-		goto out_fail;
-	}
-
-	/*
-	 * If the superblock doesn't start on a hardware sector boundary,
-	 * calculate the offset.  
-	 */
-	if (blocksize != BLOCK_SIZE) {
-		logic_sb_block = (sb_block * BLOCK_SIZE) / blocksize;
-		offset = (sb_block * BLOCK_SIZE) % blocksize;
-	} else {
-		logic_sb_block = sb_block;
-	}
+    sb->s_fs_info = sbi;
+    memset(sbi, 0, sizeof(*sbi));
+    blocksize = sb_min_blocksize(sb, BLOCK_SIZE);
+    if (!blocksize) {
+        LAB4ERROR("unable to set blocksize\n");
+        goto out_fail;
+    }
 
     /*
-    LAB4DEBUG("sb@%lu:%u\n", logic_sb_block, offset);
-    */
+     * If the superblock doesn't start on a hardware sector boundary,
+     * calculate the offset.  
+     */
+    if (blocksize != BLOCK_SIZE) {
+        logic_sb_block = (sb_block * BLOCK_SIZE) / blocksize;
+        offset = (sb_block * BLOCK_SIZE) % blocksize;
+    } else {
+        logic_sb_block = sb_block;
+    }
 
-	if (!(bh = sb_bread(sb, logic_sb_block))) {
-		LAB4ERROR("unable to read super block\n");
-		goto out_fail;
-	}
+    LAB4DEBUG("sb@%lu:%u\n", logic_sb_block, offset);
+
+    if (!(bh = sb_bread(sb, logic_sb_block))) {
+        LAB4ERROR("unable to read super block\n");
+        goto out_fail;
+    }
 
     LAB4DEBUG("finished reading block\n");
-	es = (struct lab4fs_super_block *) (((char *)bh->b_data) + offset);
-	sbi->s_sb = es;
-	sb->s_magic = le32_to_cpu(es->s_magic);
-	if (sb->s_magic != LAB4FS_SUPER_MAGIC) {
-		if (!silent)
-			LAB4ERROR("VFS: Can't find lab4fs filesystem on dev %s.\n",
-					sb->s_id);
-		goto failed_mount;
-	}
+    es = (struct lab4fs_super_block *) (((char *)bh->b_data) + offset);
+    sbi->s_sb = es;
+    sb->s_magic = le32_to_cpu(es->s_magic);
+    if (sb->s_magic != LAB4FS_SUPER_MAGIC) {
+        if (!silent)
+            LAB4ERROR("VFS: Can't find lab4fs filesystem on dev %s.\n",
+                    sb->s_id);
+        goto failed_mount;
+    }
 
     LAB4DEBUG("converting block size\n");
-	blocksize = le32_to_cpu(es->s_block_size);
-	hblock = bdev_hardsect_size(sb->s_bdev);
-	if (sb->s_blocksize != blocksize) {
-		/*
-		 * Make sure the blocksize for the filesystem is larger
-		 * than the hardware sectorsize for the machine.
-		 */
-		if (blocksize < hblock) {
-			LAB4ERROR("blocksize %d too small for "
-			       "device blocksize %d.\n", blocksize, hblock);
-			goto failed_mount;
-		}
+    blocksize = le32_to_cpu(es->s_block_size);
+    hblock = bdev_hardsect_size(sb->s_bdev);
+    if (sb->s_blocksize != blocksize) {
+        /*
+         * Make sure the blocksize for the filesystem is larger
+         * than the hardware sectorsize for the machine.
+         */
+        if (blocksize < hblock) {
+            LAB4ERROR("blocksize %d too small for "
+                    "device blocksize %d.\n", blocksize, hblock);
+            goto failed_mount;
+        }
 
-		brelse (bh);
-		sb_set_blocksize(sb, blocksize);
-		logic_sb_block = (sb_block * BLOCK_SIZE) / blocksize;
-		offset = (sb_block * BLOCK_SIZE) % blocksize;
-		bh = sb_bread(sb, logic_sb_block);
-		if (!bh) {
-			LAB4ERROR("Can't read superblock on 2nd try.\n");
-			goto failed_mount;
-		}
-		es = (struct lab4fs_super_block *)(((char *)bh->b_data) + offset);
-		sbi->s_sb = es;
-		if (es->s_magic != cpu_to_le32(LAB4FS_SUPER_MAGIC)) {
-			LAB4ERROR("Magic mismatch, very weird !\n");
-			goto failed_mount;
-		}
-	}
+        brelse (bh);
+        sb_set_blocksize(sb, blocksize);
+        logic_sb_block = (sb_block * BLOCK_SIZE) / blocksize;
+        offset = (sb_block * BLOCK_SIZE) % blocksize;
+        bh = sb_bread(sb, logic_sb_block);
+        if (!bh) {
+            LAB4ERROR("Can't read superblock on 2nd try.\n");
+            goto failed_mount;
+        }
+        es = (struct lab4fs_super_block *)(((char *)bh->b_data) + offset);
+        sbi->s_sb = es;
+        if (es->s_magic != cpu_to_le32(LAB4FS_SUPER_MAGIC)) {
+            LAB4ERROR("Magic mismatch, very weird !\n");
+            goto failed_mount;
+        }
+    }
     LAB4DEBUG("got the right block size: %dB\n", blocksize);
-	sb->s_maxbytes = lab4fs_max_size(es);
-	sbi->s_sbh = bh;
+    sb->s_maxbytes = lab4fs_max_size(es);
+    sbi->s_sbh = bh;
     sbi->s_log_block_size = log2(sb->s_blocksize);
     sbi->s_first_ino = le32_to_cpu(es->s_first_inode);
     sbi->s_inode_size = le32_to_cpu(es->s_inode_size);
