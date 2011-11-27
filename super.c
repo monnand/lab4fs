b74bcb32e4be1dc86e56e96062ec036dda95cf4d
Monnand <monnand@gmail.com>
2011-11-27 04:37:40 -0500

    fixing bug

--- super.c
+++ super.c
@@ -140,7 +140,6 @@ static int lab4fs_fill_super(struct super_block * sb, void * data, int silent)
     }
 
     LAB4DEBUG("converting block size\n");
-    return -EIO;
     blocksize = le32_to_cpu(es->s_block_size);
     hblock = bdev_hardsect_size(sb->s_bdev);
     if (sb->s_blocksize != blocksize) {
