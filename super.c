8d08e1520ee9b2f878871cff0e86327a3a06cb1e
Monnand <monnand@gmail.com>
2011-11-28 03:50:52 -0500

    fixing bug

--- super.c
+++ super.c
@@ -69,6 +69,7 @@ struct super_operations lab4fs_super_ops = {
     */
 };
 
+#if 0
 static int lab4fs_fill_super(struct super_block * sb, void * data, int silent)
 {
     struct buffer_head * bh;
@@ -204,6 +205,133 @@ static int lab4fs_fill_super(struct super_block * sb, void * data, int silent)
     }
 
     return 0;
+failed_mount:
+out_fail:
+	kfree(sbi);
+	return err;
+}
+#endif
+
+static int lab4fs_fill_super(struct super_block * sb, void * data, int silent)
+{
+    struct buffer_head * bh;
+    int blocksize = BLOCK_SIZE;
+    unsigned long logic_sb_block;
+    unsigned offset = 0;
+    unsigned long sb_block = 1;
+    struct lab4fs_super_block *es;
+    struct lab4fs_sb_info *sbi;
+    struct inode *root;
+    int hblock;
+    int err;
+
+    sbi = kmalloc(sizeof(*sbi), GFP_KERNEL);
+    if (!sbi)
+        return -ENOMEM;
+
+    sb->s_fs_info = sbi;
+    memset(sbi, 0, sizeof(*sbi));
+    blocksize = sb_min_blocksize(sb, BLOCK_SIZE);
+    if (!blocksize) {
+        LAB4ERROR("unable to set blocksize\n");
+        err = -EIO;
+        goto out_fail;
+    }
+
+    /*
+     * If the superblock doesn't start on a hardware sector boundary,
+     * calculate the offset.  
+     */
+    if (blocksize != BLOCK_SIZE) {
+        logic_sb_block = (sb_block * BLOCK_SIZE) / blocksize;
+        offset = (sb_block * BLOCK_SIZE) % blocksize;
+    } else {
+        logic_sb_block = sb_block;
+    }
+
+    if (!(bh = sb_bread(sb, logic_sb_block))) {
+        LAB4ERROR("unable to read super block\n");
+        goto out_fail;
+    }
+
+    es = (struct lab4fs_super_block *) (((char *)bh->b_data) + offset);
+    sbi->s_sb = es;
+    sb->s_magic = le32_to_cpu(es->s_magic);
+    if (sb->s_magic != LAB4FS_SUPER_MAGIC) {
+        if (!silent)
+            LAB4ERROR("VFS: Can't find lab4fs filesystem on dev %s.\n",
+                    sb->s_id);
+        goto failed_mount;
+    }
+
+    blocksize = le32_to_cpu(es->s_block_size);
+    hblock = bdev_hardsect_size(sb->s_bdev);
+    if (sb->s_blocksize != blocksize) {
+        /*
+         * Make sure the blocksize for the filesystem is larger
+         * than the hardware sectorsize for the machine.
+         */
+        if (blocksize < hblock) {
+            LAB4ERROR("blocksize %d too small for "
+                    "device blocksize %d.\n", blocksize, hblock);
+            goto failed_mount;
+        }
+
+        brelse (bh);
+        sb_set_blocksize(sb, blocksize);
+        logic_sb_block = (sb_block * BLOCK_SIZE) / blocksize;
+        offset = (sb_block * BLOCK_SIZE) % blocksize;
+        bh = sb_bread(sb, logic_sb_block);
+        if (!bh) {
+            LAB4ERROR("Can't read superblock on 2nd try.\n");
+            goto failed_mount;
+        }
+        es = (struct lab4fs_super_block *)(((char *)bh->b_data) + offset);
+        sbi->s_sb = es;
+        if (es->s_magic != cpu_to_le32(LAB4FS_SUPER_MAGIC)) {
+            LAB4ERROR("Magic mismatch, very weird !\n");
+            goto failed_mount;
+        }
+    }
+    sb->s_maxbytes = lab4fs_max_size(es);
+    sbi->s_sbh = bh;
+    sbi->s_log_block_size = log2(sb->s_blocksize);
+    sbi->s_first_ino = le32_to_cpu(es->s_first_inode);
+    sbi->s_inode_size = le32_to_cpu(es->s_inode_size);
+    sbi->s_log_inode_size = log2(sbi->s_inode_size);
+    sbi->s_inode_table = le32_to_cpu(es->s_inode_table);
+    sbi->s_data_blocks = le32_to_cpu(es->s_data_blocks);
+
+    sbi->s_inode_bitmap.nr_valid_bits = le32_to_cpu(es->s_inodes_count);
+    sbi->s_data_bitmap.nr_valid_bits = le32_to_cpu(es->s_blocks_count) 
+        - le32_to_cpu(es->s_data_blocks);
+
+    sb->s_op = &lab4fs_super_ops;
+
+    err = bitmap_setup(&sbi->s_inode_bitmap, sb, le32_to_cpu(es->s_inode_bitmap));
+    if (err)
+        goto out_fail;
+    err = bitmap_setup(&sbi->s_data_bitmap, sb, le32_to_cpu(es->s_data_bitmap));
+    if (err)
+        goto out_fail;
+
+    print_super(es);
+    sbi->s_root_inode = le32_to_cpu(es->s_root_inode);
+    LAB4DEBUG("Now loading root dir\n");
+    root = iget(sb, sbi->s_root_inode);
+    if (root == NULL) {
+        err = -EIO;
+        goto failed_mount;
+    }
+    sb->s_root = d_alloc_root(root);
+    if (!sb->s_root) {
+        iput(root);
+        kfree(sbi);
+        return -ENOMEM;
+    }
+
+    return 0;
+
 failed_mount:
 out_fail:
 	kfree(sbi);
