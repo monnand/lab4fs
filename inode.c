c6245505671d9dbfcddeddf88faab6df183662ec
Monnand <monnand@gmail.com>
2011-11-28 05:14:29 -0500

    debugging...

--- inode.c
+++ inode.c
@@ -377,12 +377,7 @@ struct inode *lab4fs_new_inode(struct inode *dir, int mode)
     }
     read_unlock(&sbi->rwlock);
 
-    LAB4DEBUG("I will try to create a new inode\n");
     ino = bitmap_find_next_zero_bit(&sbi->s_inode_bitmap, sbi->s_first_ino, 1);
-    LAB4DEBUG("new inode number: %u\n", ino);
-    LAB4DEBUG("OK, I have to stop here\n");
-    err = -ENOSPC;
-	return ERR_PTR(err);
 
     if (ino >= sbi->s_inodes_count || ino < sbi->s_first_ino) {
         err = -ENOSPC;
@@ -408,6 +403,10 @@ struct inode *lab4fs_new_inode(struct inode *dir, int mode)
 	ei->i_dir_acl = 0;
 	inode->i_generation = sbi->s_next_generation++;
 
+    LAB4DEBUG("new inode number: %u\n", ino);
+    LAB4DEBUG("OK, I have to stop here\n");
+    err = -ENOSPC;
+	return ERR_PTR(err);
 	insert_inode_hash(inode);
     mark_inode_dirty(inode);
 
