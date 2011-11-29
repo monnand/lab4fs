18a048027d396f55bc32f782124f71f65a06d2d4
Monnand <monnand@gmail.com>
2011-11-29 02:55:51 -0500

    debugging...

--- inode.c
+++ inode.c
@@ -254,6 +254,14 @@ static __u32 lab4fs_alloc_data_block(struct inode *inode, __u32 perfered, long *
 {
 	struct super_block *sb = inode->i_sb;
     struct lab4fs_sb_info *sbi = LAB4FS_SB(sb);
+
+#ifdef CONFIG_LAB4FS_DEBUG
+    if (inode->i_ino == LAB4FS_ROOT_INO) {
+        LAB4DEBUG("Root need more data block, Here is the data bitmap:\n");
+        print_buffer_head(sbi->s_data_bitmap.bhs[0], 0, 8);
+        LAB4DEBUG("END\n");
+    }
+#endif
     __u32 start = perfered - sbi->s_data_blocks;
     __u32 found = bitmap_find_next_zero_bit(&sbi->s_data_bitmap, start, 1);
 
